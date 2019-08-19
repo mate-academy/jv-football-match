050d161efebf7a73504920a6a0c46ca4ba5ec1d6
Taranarnik <pavelkurilyuk97@gmail.com>
2019-08-20 01:25:38 +0300

    added fixed solution for football match homework

--- src/main/java/core/basesyntax/FootballMatch.java
+++ src/main/java/core/basesyntax/FootballMatch.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 public class FootballMatch {
     /**
      * This program is
-     * my first homework.
+     * my first homework at Mate Academy.
      */
     public int matchResult(int result1, int result2, int bet1, int bet2) {
         return result1 == result2 ? bet1 == bet2 ? bet1 == result1 ? 2 : 1 : 0

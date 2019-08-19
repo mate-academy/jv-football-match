8c9ca354fcd075ef72fba4a62e05edd16795e7e3
RomanMinevich <52323566+RomanMinevich@users.noreply.github.com>
2019-08-19 23:12:54 +0300

    Update FootballMatch.java

--- src/main/java/core/basesyntax/FootballMatch.java
+++ src/main/java/core/basesyntax/FootballMatch.java
@@ -6,9 +6,9 @@ public class FootballMatch {
      * С помощью тернарного оператора написать функцию, которая будет считать исход матча.
      * Метод будет принимать 4 параметра и возвращать целое число.
      * 4 параметра метода - результат матча и то, какую ставку сделал пользователь.
-     *
+     * <p>
      * Пример: 0,2,1,1. Что значит: команды сыграли в счёт 0:2, а пользователь ставил 1:1.
-     *
+     *<p>
      * Задача:
      *         если пользователь угадал 100% результат - возвращать 2
      *         если угадал, что выиграла какая-либо команда
@@ -17,9 +17,9 @@ public class FootballMatch {
      *         если не угадал ничего - возвращать 0.
      */
     public int matchResult(int result1, int result2, int bet1, int bet2) {
-        int answer = 0;
-        answer = (result1 > result2 && bet1 > bet2 || result1 < result2 && bet1 < bet2) ? 1 : answer;
-        answer = (result1 + result2 == bet1 + bet2) ? 2 : answer;
-        return answer;
+        int win = 0;
+        win = (result1 > result2 && bet1 > bet2 || result1 < result2 && bet1 < bet2) ? 1 : answer;
+        win = (result1 + result2 == bet1 + bet2) ? 2 : answer;
+        return win;
     }
 }

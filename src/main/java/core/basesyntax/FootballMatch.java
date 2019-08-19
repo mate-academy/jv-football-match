9190f8e597476d068bc7ab4667aaf8f9c6c4de6a
ghkhrshch <george.khruschov@gmail.com>
2019-08-19 18:41:45 +0300

    Corrected 'Line 21 is longer than 100 characters (found 102).'

--- src/main/java/core/basesyntax/FootballMatch.java
+++ src/main/java/core/basesyntax/FootballMatch.java
@@ -18,7 +18,8 @@ public class FootballMatch {
      */
     public int matchResult(int result1, int result2, int bet1, int bet2) {
 
-        int playerResult = (bet1 > bet2 && result1 > result2) || (bet1 < bet2 && result1 < result2) ||
+        int playerResult = (bet1 > bet2 && result1 > result2) ||
+                (bet1 < bet2 && result1 < result2) ||
                 (bet1 == bet2 && result1 == result2) ?
                 (bet1 == result1 && bet2 == result2) ? 2 : 1: 0;
 

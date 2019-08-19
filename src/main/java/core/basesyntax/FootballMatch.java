7998d396bc4732074be4962ace47e6d8ece8585f
RomanMinevich <52323566+RomanMinevich@users.noreply.github.com>
2019-08-19 23:45:05 +0300

    Update FootballMatch.java

--- src/main/java/core/basesyntax/FootballMatch.java
+++ src/main/java/core/basesyntax/FootballMatch.java
@@ -18,9 +18,9 @@ public class FootballMatch {
      */
     public int matchResult(int result1, int result2, int bet1, int bet2) {
         int win = 0;
-        answer = (result1 > result2 && bet1 > bet2
+        win = (result1 > result2 && bet1 > bet2
                 || result1 < result2 && bet1 < bet2
-                || result1 == result2 && bet1 == bet2) ? 1 : answer;
+                || result1 == result2 && bet1 == bet2) ? 1 : win;
         win = (result1 + result2 == bet1 + bet2) ? 2 : win;
         return win;
     }

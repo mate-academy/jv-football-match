d1629d960bbf8152e1ba42e16bcd9d919400aafe
Rosinskyi <sergey22061993@gmail.com>
2019-08-19 15:36:16 +0300

    added solution for FootballMatch homework

--- src/main/java/core/basesyntax/FootballMatch.java
+++ src/main/java/core/basesyntax/FootballMatch.java
@@ -16,9 +16,9 @@ public class FootballMatch {
      */
     public int matchResult(int result1, int result2, int bet1, int bet2) {
 
-        int result = result1 == bet1 && result2 == bet2 ? 2 : result1 == result2 && bet1 == bet2
+
+        return result1 == bet1 && result2 == bet2 ? 2 : result1 == result2 && bet1 == bet2
                 || result1 < result2 && bet1 < bet2
                 || result1 > result2 && bet1 > bet2 ? 1 : 0;
-        return result;
     }
 }

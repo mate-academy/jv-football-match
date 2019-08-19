46577496ec340f5c3a4a0bce016d01f94ddaf230
Iurii <kenu21@gmail.com>
2019-08-20 00:04:02 +0300

    added hw 1 - solution

--- src/main/java/core/basesyntax/FootballMatch.java
+++ src/main/java/core/basesyntax/FootballMatch.java
@@ -17,6 +17,20 @@ public class FootballMatch {
      *         если не угадал ничего - возвращать 0.
      */
     public int matchResult(int result1, int result2, int bet1, int bet2) {
-        return 0;
+        if(result1 == bet1 && result2 == bet2) {
+            return 2;
+        }
+        else if(result1 > result2 && bet1 > bet2) {
+            return 1;
+        }
+        else if(result1 < result2 && bet1 < bet2) {
+            return 1;
+        }
+        else if(result1 == result2 && bet1 == bet2) {
+            return 1;
+        }
+        else {
+            return 0;
+        }
     }
 }

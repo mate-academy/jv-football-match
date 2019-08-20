0caaf2fa4af74ca0e2ae68aac90820216849ff22
lenovo <suleimanovayev@gmail.com>
2019-08-20 17:15:40 +0300

    solution sport match

--- src/main/java/core/basesyntax/FootballMatch.java
+++ src/main/java/core/basesyntax/FootballMatch.java
@@ -1,6 +1,5 @@
 package core.basesyntax;
-
-public class FootballMatch {
+ class FootballMatch {
 
     /**
      * С помощью тернарного оператора написать функцию, которая будет считать исход матча.
@@ -17,6 +16,9 @@ public class FootballMatch {
      *         если не угадал ничего - возвращать 0.
      */
     public int matchResult(int result1, int result2, int bet1, int bet2) {
-        return 0;
+        int result = 0;
+        result = (result1 == result2 && bet1 == bet2 || result1 > result2 && bet1 > bet2 || result1 < result2 && bet1 < bet2) ? 1 : result;
+        result = (result1 == bet1 && result2 == bet2) ? 2 : result;
+        return result;
     }
 }

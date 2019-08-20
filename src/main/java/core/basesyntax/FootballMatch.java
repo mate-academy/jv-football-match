82351986308083f15828f28b6cd8d1e2d1456a5b
ghkhrshch <george.khruschov@gmail.com>
2019-08-20 14:25:40 +0300

    readability improved

--- src/main/java/core/basesyntax/FootballMatch.java
+++ src/main/java/core/basesyntax/FootballMatch.java
@@ -18,9 +18,11 @@ public class FootballMatch {
      */
     public int matchResult(int result1, int result2, int bet1, int bet2) {
         return (bet1 == result1 && bet2 == result2)
-                    ? 2 : ((bet1 > bet2 && result1 > result2)
-                          || (bet1 < bet2 && result1 < result2)
-                          || (bet1 == bet2 && result1 == result2))
-                                ? 1 : 0;
+                    ? 2
+                    : ((bet1 > bet2 && result1 > result2)
+                      || (bet1 < bet2 && result1 < result2)
+                      || (bet1 == bet2 && result1 == result2))
+                        ? 1
+                        : 0;
     }
 }

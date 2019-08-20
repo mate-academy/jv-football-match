7e92d2e17dbf360afff9de4b272a6025347021ca
ghkhrshch <george.khruschov@gmail.com>
2019-08-20 11:22:22 +0300

    readability fixed, probably correctly.

--- src/main/java/core/basesyntax/FootballMatch.java
+++ src/main/java/core/basesyntax/FootballMatch.java
@@ -17,7 +17,6 @@ public class FootballMatch {
      *         если не угадал ничего - возвращать 0.
      */
     public int matchResult(int result1, int result2, int bet1, int bet2) {
-
         return (bet1 == result1 && bet2 == result2)
                     ? 2 : ((bet1 > bet2 && result1 > result2)
                           || (bet1 < bet2 && result1 < result2)

2b8cc7a7b208b64c86dc2b752d43890d88753153
RostykRosolovskyi <38504193+RostykRosolovskyi@users.noreply.github.com>
2019-08-20 18:30:35 +0300

    added solution for method matchresalt

--- src/main/java/core/basesyntax/FootballMatch.java
+++ src/main/java/core/basesyntax/FootballMatch.java
@@ -15,11 +15,10 @@ public class FootballMatch {
      * если не угадал ничего - возвращать 0.
      */
     public int matchResult(int result1, int result2, int bet1, int bet2) {
-        int s = (result1 == bet1 && result2 == bet2) ? 2 : ((result1 > result2 && bet1 > bet2
+        return (result1 == bet1 && result2 == bet2) ? 2 : ((result1 > result2 && bet1 > bet2
                 || result1 == result2 && bet1 == bet2
                 || result1 < result2 && bet1 < bet2) ? 1 : 0);
 
 
-        return s;
     }
 }

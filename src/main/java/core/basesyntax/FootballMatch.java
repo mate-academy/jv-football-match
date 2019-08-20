6d1f93d9167c3e54d8f2f2933c2925ec10dc58b5
Taranarnik <pavelkurilyuk97@gmail.com>
2019-08-20 11:27:32 +0300

    added non complex solution for football match homework

--- src/main/java/core/basesyntax/FootballMatch.java
+++ src/main/java/core/basesyntax/FootballMatch.java
@@ -6,8 +6,9 @@ public class FootballMatch {
      * my first homework at Mate Academy.
      */
     public int matchResult(int result1, int result2, int bet1, int bet2) {
-        return result1 == result2 ? bet1 == bet2 ? bet1 == result1 ? 2 : 1 : 0
-            : result1 > result2 ? bet1 > bet2 ? bet1 == result1 ? bet2 == result2 ? 2 : 1 : 1 : 0
-            : bet2 > bet1 ? bet2 == result2 ? bet1 == result1 ? 2 : 1 : 1 : 0;
+        return result1 == bet1 && result2 == bet2 ? 2
+                : result1 == result2 && bet1 == bet2
+                || result1 < result2 && bet1 < bet2
+                || result1 > result2 && bet1 > bet2 ? 1 : 0;
     }
 }

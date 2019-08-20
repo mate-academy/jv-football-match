6ef7b96ba05c86d848b1de7e742c358e16d203d7
Саша Бунин <sasha192.bunin@gmail.com>
2019-08-20 10:52:26 +0300

    Update FootballMatch.java

--- src/main/java/core/basesyntax/FootballMatch.java
+++ src/main/java/core/basesyntax/FootballMatch.java
@@ -1,13 +1,11 @@
-
+package core.basesyntax;
 
 public class FootballMatch {
+    public int matchResult(int result1, int result2, int bet1, int bet2) {
 
-
-	public int matchResult(int result1, int result2, int bet1, int bet2){
-
-		return result1 - bet1 + result2 - bet2 == 0 ? 2 : ( result1 - result2 ) * ( bet1 - bet2 ) > 0 ? 1 : 0 ;
-
-	}
+        boolean boolVar = result1 - bet1 + result2 - bet2 == 0;
+        return boolVar ? 2 : (result1 - result2) * (bet1 - bet2) > 0 ? 1 : 0;
+    }
 
 
 }

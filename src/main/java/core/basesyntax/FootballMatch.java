b72ac5e7a20321f09561c10e916cde6f17461155
ghkhrshch <george.khruschov@gmail.com>
2019-08-20 11:21:05 +0300

    readability fixed, probably correctly.

--- src/main/java/core/basesyntax/FootballMatch.java
+++ src/main/java/core/basesyntax/FootballMatch.java
@@ -18,12 +18,10 @@ public class FootballMatch {
      */
     public int matchResult(int result1, int result2, int bet1, int bet2) {
 
-        int playerResult =
-                (bet1 > bet2 && result1 > result2) || (bet1 < bet2 && result1 < result2)
-                 || (bet1 == bet2 && result1 == result2)
-                        ? (bet1 == result1 && bet2 == result2)
-                            ? 2 : 1 : 0;
-
-        return playerResult;
+        return (bet1 == result1 && bet2 == result2)
+                    ? 2 : ((bet1 > bet2 && result1 > result2)
+                          || (bet1 < bet2 && result1 < result2)
+                          || (bet1 == bet2 && result1 == result2))
+                                ? 1 : 0;
     }
 }

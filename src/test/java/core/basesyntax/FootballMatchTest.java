6b4fcac30b7e1280af0afe3657ed4d49c2a54059
Taras Khalak <tarasulo@gmail.com>
2019-08-21 11:39:36 +0300

    'Solution'

--- src/test/java/core/basesyntax/FootballMatchTest.java
+++ src/test/java/core/basesyntax/FootballMatchTest.java
@@ -7,20 +7,48 @@ public class FootballMatchTest {
 
     @Test
     public void matchScore() {
-        FootballMatch footballMatch = new FootballMatch();
+        core.basesyntax.FootballMatch footballMatch = new core.basesyntax.FootballMatch();
         for (int result1 = 1; result1 < 5; result1++) {
             for (int result2 = 1; result2 < 5; result2++) {
-                for (int bet1 = 1; bet1 < 5; bet1++) {
-                    for (int bet2 = 1; bet2 < 5; bet2++) {
-                        int actualResult =
-                                footballMatch.matchResult(result1, result2, bet1, bet2);
-                        int expectedResult
-                                = footballMatchResult(result1, result2, bet1, bet2);
+                for (int bet2 = 1; bet2 < 5; bet2++) {
+                    int actualResult =
+                            footballMatch.matchResult(result1, result2, 1, bet2);
+                    int expectedResult
+                            = footballMatchResult(result1, result2, 1, bet2);
 
-                        Assert.assertEquals("Test failed with parameters = "
-                                + result1 + ", " + result2 + ", "
-                                + bet1 + ", " + bet2, expectedResult, actualResult);
-                    }
+                    Assert.assertEquals("Test failed with parameters = "
+                            + result1 + ", " + result2 + ", "
+                            + 1 + ", " + bet2, expectedResult, actualResult);
+                }
+                for (int bet2 = 1; bet2 < 5; bet2++) {
+                    int actualResult =
+                            footballMatch.matchResult(result1, result2, 2, bet2);
+                    int expectedResult
+                            = footballMatchResult(result1, result2, 2, bet2);
+
+                    Assert.assertEquals("Test failed with parameters = "
+                            + result1 + ", " + result2 + ", "
+                            + 2 + ", " + bet2, expectedResult, actualResult);
+                }
+                for (int bet2 = 1; bet2 < 5; bet2++) {
+                    int actualResult =
+                            footballMatch.matchResult(result1, result2, 3, bet2);
+                    int expectedResult
+                            = footballMatchResult(result1, result2, 3, bet2);
+
+                    Assert.assertEquals("Test failed with parameters = "
+                            + result1 + ", " + result2 + ", "
+                            + 3 + ", " + bet2, expectedResult, actualResult);
+                }
+                for (int bet2 = 1; bet2 < 5; bet2++) {
+                    int actualResult =
+                            footballMatch.matchResult(result1, result2, 4, bet2);
+                    int expectedResult
+                            = footballMatchResult(result1, result2, 4, bet2);
+
+                    Assert.assertEquals("Test failed with parameters = "
+                            + result1 + ", " + result2 + ", "
+                            + 4 + ", " + bet2, expectedResult, actualResult);
                 }
             }
         }

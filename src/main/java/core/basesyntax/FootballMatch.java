93a3abb9dded01773911b298dc5b9daacf64a34c
Mykhaylo12 <55308826+Mykhaylo12@users.noreply.github.com>
2019-12-02 17:09:27 +0200

    deleted comments

--- src/main/java/core/basesyntax/FootballMatch.java
+++ src/main/java/core/basesyntax/FootballMatch.java
@@ -15,9 +15,9 @@ public class FootballMatch {
      * если не угадал ничего - возвращать 0.</p>
      */
     public int matchResult(int result1, int result2, int bet1, int bet2) {
-        return ((result1 == bet1) && (result2 == bet2)) ? 2 : /* 100% gues*/
-                ((result1 > result2) && (bet1 > bet2)) ? 1 :    /* first team won*/
-                 ((result1 < result2) && (bet1 < bet2)) ? 1 :      /* second team won*/
-                    ((result1 == result2) && (bet1 == bet2)) ? 1 : 0; /* second team won*/
+        return ((result1 == bet1) && (result2 == bet2)) ? 2 :
+                ((result1 > result2) && (bet1 > bet2)) ? 1 :
+                 ((result1 < result2) && (bet1 < bet2)) ? 1 : 
+                    ((result1 == result2) && (bet1 == bet2)) ? 1 : 0;
     }
 }

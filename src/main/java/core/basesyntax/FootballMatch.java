dc910ca74d3d2d277090a1cb4c8312faec1c9ba9
Volodymyr <v.shubin12318@gmail.com>
2019-12-02 14:14:27 +0200

    added solution match result homework

--- src/main/java/core/basesyntax/FootballMatch.java
+++ src/main/java/core/basesyntax/FootballMatch.java
@@ -15,6 +15,13 @@ public class FootballMatch {
      * если не угадал ничего - возвращать 0.</p>
      */
     public int matchResult(int result1, int result2, int bet1, int bet2) {
-        return 0;
+        int result = 0;
+        if (result1 == bet1 && result2 == bet2) {
+            result = 2;
+        } else if (result1 < result2 && bet1 < bet2
+                || result1 > result2 && bet1 > bet2) {
+            result = 1;
+        }
+        return result;
     }
 }

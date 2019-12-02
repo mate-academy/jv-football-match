aa804d3689f5a8507dd9b32000ee70bb072ddeee
Dmytro Hryhoruk <dimamyhgrygoruk@gmail.com>
2019-12-02 13:22:23 +0200

    added correction

--- src/main/java/core/basesyntax/FootballMatch.java
+++ src/main/java/core/basesyntax/FootballMatch.java
@@ -15,11 +15,11 @@ public class FootballMatch {
      * если не угадал ничего - возвращать 0.</p>
      */
     public int matchResult(int result1, int result2, int bet1, int bet2) {
-        int returnStatement = ((result1 == bet1) && (result2 == bet2))? 2 :
-                ((result1 == result2 && bet1 == bet2
-                || result1 < result2 && bet1 < bet2
-                || result1 > result2 && bet1 > bet2)) ?
-                1: 0;
-        return returnStatement;
+        return((result1 == bet1) && (result2 == bet2))
+                ? 2 : ((result1 == result2 && bet1 == bet2)
+                      || (result1 < result2 && bet1 < bet2)
+                      || (result1 > result2 && bet1 > bet2))
+                ? 1 : 0;
+
     }
 }

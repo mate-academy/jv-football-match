650065563eb2a5ed4940ee80e72f711e6afd5f66
IvanKrivoruchko <krivoruchko.ivan.ukr.net>
2019-12-02 16:58:50 +0200

    added homework11 solution

--- src/main/java/core/basesyntax/FootballMatch.java
+++ src/main/java/core/basesyntax/FootballMatch.java
@@ -15,7 +15,9 @@ public class FootballMatch {
      * если не угадал ничего - возвращать 0.</p>
      */
     public int matchResult(int result1, int result2, int bet1, int bet2) {
-        return bet1 == result1 && bet2 == result2 ?  2 : bet1 > bet2 && result1 > result2
-                || bet2 > bet1 && result2 > result1 || bet1 == bet2 && result1 == result2 ? 1 : 0;
+        return bet1 == result1 && bet2 == result2 ?  2 :
+                bet1 > bet2 && result1 > result2
+                || bet2 > bet1 && result2 > result1
+                        || bet1 == bet2 && result1 == result2 ? 1 : 0;
     }
 }

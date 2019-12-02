6d8c0a4adf885f67c72d60b0772cf460ca86b787
Mykhailo Kramar <3805033@gmail.com>
2019-12-02 16:55:23 +0200

    home work 1.1 with  solution final

--- src/main/java/core/basesyntax/FootballMatch.java
+++ src/main/java/core/basesyntax/FootballMatch.java
@@ -17,7 +17,7 @@ public class FootballMatch {
     public int matchResult(int result1, int result2, int bet1, int bet2) {
         return ((result1 == bet1) && (result2 == bet2)) ? 2 : /* 100% gues*/
                 ((result1 > result2) && (bet1 > bet2)) ? 1 :    /* first team won*/
-                        ((result1 < result2) && (bet1 < bet2)) ? 1 :      /* second team won*/
-                                ((result1 == result2) && (bet1 == bet2)) ? 1 : 0;
+                ((result1 < result2) && (bet1 < bet2)) ? 1 :      /* second team won*/
+                    ((result1 == result2) && (bet1 == bet2)) ? 1 : 0; /* second team won*/
     }
 }

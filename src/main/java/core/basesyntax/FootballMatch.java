9b8adbc47d5a7f73efe2b827be6720be0f6a9160
Andrey Tanchak <61573898+boterf@users.noreply.github.com>
2020-03-30 15:19:46 +0300

    added  solution with trinar operator

--- src/main/java/core/basesyntax/FootballMatch.java
+++ src/main/java/core/basesyntax/FootballMatch.java
@@ -1,43 +1,25 @@
 package core.basesyntax;
 
 public class FootballMatch {
-    /**
- * <p>С помощью тернарного оператора написать функцию, которая будет считать исход матча.
- * Метод будет принимать 4 параметра и возвращать целое число.
- * 4 параметра метода - результат матча и то, какую ставку сделал пользователь.
- * Пример: 0,2,1,1. Что значит: команды сыграли в счёт 0:2, а пользователь ставил 1:1.
- * Задача:
- * если пользователь угадал 100% результат - возвращать 2
- * если угадал, что выиграла какая-либо команда
- * (пр. сыграли 1:2, а ставил 0:3 - то пользователь у выиграше)
- * то возвращать 1
- * если не угадал ничего - возвращать 0.</p>
- */
-    public static void main(String[] args) {
-        System.out.println(matchResult(1,1,1,0));
+     public static void main(String[] args) {
+        System.out.println(matchResult(1,2,1,1));
     }
 
     public static int matchResult(int result1, int result2, int bet1, int bet2) {
-        int winr = 0;
-        int betr = 0;
-        if (result1 > result2) {
-            winr = 1;
-        } else if (result1 < result2) {
-            winr = 2;
-        }
-        if (bet1 > bet2) {
-            betr = 1;
-        } else if (bet1 < bet2) {
-            betr = 2;
-        }
-        if (result1 == bet1 && result2 == bet2) {
-            return 2;
-        }
-        if (betr == winr) {
-            return 1;
-        } else {
-            return 0;
-        }
+        int winr;
+        int betr;
+        betr = bet1 > bet2
+                ? 1
+                : bet1 < bet2
+                ? 2 : 0;
+        winr = result1 > result2
+                ? 1
+                : result1 < result2
+                ? 2 : 0;
+        return (result1 == bet1 && result2 == bet2)
+                ? 2
+                : (betr == winr)
+                ? 1
+                : 0;
     }
-}
 

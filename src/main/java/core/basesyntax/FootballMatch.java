64b9510270de0cf0e7f104ee906a3c9f82c0d9f1
6ALLIKA <skorik009@gmail.com>
2020-03-30 13:48:30 +0300

    исправлен вид, под тернарный способ

--- src/main/java/core/basesyntax/FootballMatch.java
+++ src/main/java/core/basesyntax/FootballMatch.java
@@ -15,14 +15,9 @@ public class FootballMatch {
      * если не угадал ничего - возвращать 0.</p>
      */
     public int matchResult(int result1, int result2, int bet1, int bet2) {
-        if (result1 == bet1 && result2 == bet2) {
-            return 2;
-        }
-        if (result1 > result2 && bet1 > bet2
+        return result1 == bet1 && result2 == bet2
+                ? 2 : result1 > result2 && bet1 > bet2
                 || result1 < result2 && bet1 < bet2
-                || result1 == result2 && bet1 == bet2) {
-            return 1;
-        }
-        return 0;
+                || result1 == result2 && bet1 == bet2 ? 1 : 0;
     }
 }

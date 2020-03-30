5675f9225db85c76a90330932e6e5a6fc8b1cc9d
Rostislav Levadnyi <rlevadniy@ukr.net>
2020-03-30 16:51:16 +0300

    fixed content inside FootballMatch.class

--- src/main/java/core/basesyntax/FootballMatch.java
+++ src/main/java/core/basesyntax/FootballMatch.java
@@ -1,10 +1,6 @@
 package core.basesyntax;
 
 public class FootballMatch {
-    public static void main(String[] args) {
-        FootballMatch match = new FootballMatch();
-        System.out.println(match.matchResult(2, 1, 1, 1));
-    }
 
     /**
      * <p>С помощью тернарного оператора написать функцию, которая будет считать исход матча.

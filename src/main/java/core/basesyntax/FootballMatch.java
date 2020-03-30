1b26abaa9587e0a2f7d626f31b6fc8740b9992c1
Andrey Tanchak <61573898+boterf@users.noreply.github.com>
2020-03-30 17:12:31 +0300

    Update FootballMatch.java

--- src/main/java/core/basesyntax/FootballMatch.java
+++ src/main/java/core/basesyntax/FootballMatch.java
@@ -1,24 +1,21 @@
 package core.basesyntax;
 
 public class FootballMatch {
-     public static void main(String[] args) {
-        System.out.println(matchResult(1,2,1,1));
-    }
 
     public static int matchResult(int result1, int result2, int bet1, int bet2) {
-        int winr;
-        int betr;
-        betr = bet1 > bet2
+      int betr = bet1 > bet2
                 ? 1
                 : bet1 < bet2
                 ? 2 : 0;
-        winr = result1 > result2
+        int winr = result1 > result2
                 ? 1
                 : result1 < result2
                 ? 2 : 0;
         return (result1 == bet1 && result2 == bet2)
                 ? 2
-                : (betr == winr)
+                : result1 < result2 && bet1 < bet2
+                || result1 > result2 && bet1 > bet2
+                || result1 == result2 && bet1 == bet2
                 ? 1
                 : 0;
     }

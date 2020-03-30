1b8e4f8eac30b70226404af30db58a05f033281f
Superman696 <61060058+Superman696@users.noreply.github.com>
2020-03-30 14:11:29 +0300

    return match result

--- src/main/java/core/basesyntax/FootballMatch.java
+++ src/main/java/core/basesyntax/FootballMatch.java
@@ -15,6 +15,10 @@ public class FootballMatch {
      * если не угадал ничего - возвращать 0.</p>
      */
     public int matchResult(int result1, int result2, int bet1, int bet2) {
-        return 0;
+        int total = (bet1 > bet2) && (result1 > result2) ? 1 : 0;
+        total = (bet1 < bet2) && (result1 < result2) ? 1 : total;
+        total = (bet1 == bet2) && (result1 == result2) ? 1 : total;
+        total = (result1 + " " + result2).equals(bet1 + " " + bet2) ? 2 : total;
+        return total;
     }
 }

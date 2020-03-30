e3f68ecae2c638c5183a4b77b3b4806497202979
Allduiin <vovapro01@gmail.com>
2020-03-30 13:41:19 +0300

    Added solution

--- src/main/java/core/basesyntax/FootballMatch.java
+++ src/main/java/core/basesyntax/FootballMatch.java
@@ -1,7 +1,6 @@
 package core.basesyntax;
 
 public class FootballMatch {
-
     /**
      * <p>С помощью тернарного оператора написать функцию, которая будет считать исход матча.
      * Метод будет принимать 4 параметра и возвращать целое число.
@@ -15,6 +14,13 @@ public class FootballMatch {
      * если не угадал ничего - возвращать 0.</p>
      */
     public int matchResult(int result1, int result2, int bet1, int bet2) {
+        if (result1 == bet1 && result2 == bet2) {
+            return 2;
+        } else if ((result1 < result2 && bet1 < bet2) || (result1 > result2 && bet1 > bet2)) {
+            return 1;
+        } else if (result1 == result2 && bet1 == bet2) {
+            return 1;
+        }
         return 0;
     }
 }

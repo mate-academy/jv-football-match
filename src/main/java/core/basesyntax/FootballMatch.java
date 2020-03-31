91e5eccc12ba9ea96a866279ee8633281e7b1bf0
Svitlana Mykolaienko <svetlana.mikolaenko@gmail.com>
2020-03-31 11:05:58 +0300

    remove result

--- src/main/java/core/basesyntax/FootballMatch.java
+++ src/main/java/core/basesyntax/FootballMatch.java
@@ -16,10 +16,9 @@ public class FootballMatch {
      */
 
     public int matchResult(int result1, int result2, int bet1, int bet2) {
-        int result = (bet1 == result1 & bet2 == result2) ? 2 :
-                    (result1 == result2 && bet1 == bet2
+        return (bet1 == result1 & bet2 == result2) ? 2 :
+                (result1 == result2 && bet1 == bet2
                         || result1 < result2 && bet1 < bet2
                         || result1 > result2 && bet1 > bet2) ? 1 : 0;
-        return result;
     }
 }

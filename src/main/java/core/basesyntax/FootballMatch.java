bc37a8318319c90ad5781583ddd55ccf721cf0a4
vladd <vladdyachkin11@gmail.com>
2020-07-27 13:30:11 +0300

    added solution2

--- src/main/java/core/basesyntax/FootballMatch.java
+++ src/main/java/core/basesyntax/FootballMatch.java
@@ -1,7 +1,5 @@
 package core.basesyntax;
 
-import java.util.Scanner;
-
 public class FootballMatch {
 
     /**
@@ -17,22 +15,25 @@ public class FootballMatch {
      * если не угадал ничего - возвращать 0.</p>
      */
     public int matchResult(int result1, int result2, int bet1, int bet2) {
-        int guess, maxResult, maxBet;
-        maxResult = result1 > result2 ? result1 : result2;
+        int guess;
+        int maxRes;
+        int maxBet;
+        maxRes = result1 > result2 ? result1 : result2;
         maxBet = bet1 > bet2 ? bet1 : bet2;
-        if((result1 == result2 && bet1 == bet2) && (result1 != bet1 && result2 != bet2)) {
+        if ((result1 == result2 && bet1 == bet2) && (result1 != bet1 && result2 != bet2)) {
             guess = 1;
         } else {
             if (result1 == bet1 && result2 == bet2) {
                 guess = 2;
             } else {
-                if ((maxResult == result1 && maxBet == bet1 || maxResult == result2 && maxBet == bet2) && (result1 != result2 && bet1 != bet2)) {
+                if ((maxRes == result1 && maxBet == bet1 || maxRes == result2 && maxBet == bet2)
+                        && (result1 != result2 && bet1 != bet2)) {
                     guess = 1;
                 } else {
                     guess = 0;
                 }
             }
         }
-            return guess;
+        return guess;
     }
 }

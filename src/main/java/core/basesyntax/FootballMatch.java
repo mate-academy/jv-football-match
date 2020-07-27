a191eaa3d1660e9b3b144f87fa199e1a2af03819
Artem Vremere <vremere_a@ukr.net>
2020-07-27 13:03:16 +0300

    added solution

--- src/main/java/core/basesyntax/FootballMatch.java
+++ src/main/java/core/basesyntax/FootballMatch.java
@@ -15,6 +15,10 @@ public class FootballMatch {
      * если не угадал ничего - возвращать 0.</p>
      */
     public int matchResult(int result1, int result2, int bet1, int bet2) {
-        return 0;
+
+        return result1 == bet1 && result2 == bet2 ? 2 :
+                result1 == result2 && bet1 == bet2
+                        || result1 > result2 && bet1 > bet2
+                        || result1 < result2 && bet1 < bet2 ? 1 : 0;
     }
 }

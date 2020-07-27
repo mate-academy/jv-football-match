ce3c61e7f415723fa44e9e39bb480c04e16278a3
margarita-ignatieva <ignatevamargarita950@gmail.com>
2020-07-27 17:33:03 +0300

    redundant lines & variables removed

--- src/main/java/core/basesyntax/FootballMatch.java
+++ src/main/java/core/basesyntax/FootballMatch.java
@@ -3,15 +3,12 @@ package core.basesyntax;
 public class FootballMatch {
 
     public int matchResult(int result1, int result2, int bet1, int bet2) {
-
-        int resultEqual = result1 == bet1 && result2 == bet2 ? 2 : 0;
-
-        int resultPart = result1 == result2 && bet1 == bet2
+        return (result1 == bet1 && result2 == bet2 ? 2 : 0) > (result1 == result2 && bet1 == bet2
                 || result1 < result2 && bet1 < bet2
-                || result1 > result2 && bet1 > bet2 ? 1 : 0;
-
-        int result = resultEqual > resultPart ? resultEqual : resultPart;
-
-        return result;
+                || result1 > result2 && bet1 > bet2 ? 1 : 0)
+                ? (result1 == bet1 && result2 == bet2 ? 2 : 0) :
+                ((result1 == result2 && bet1 == bet2
+                || result1 < result2 && bet1 < bet2
+                || result1 > result2 && bet1 > bet2 ? 1 : 0));
     }
 }

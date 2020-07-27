ba8b4bb9e6050115b6750844a159937751f36f31
Vlad Kuzmin <vladkuzmin22@gmail.com>
2020-07-27 16:51:15 +0300

    hw-1.1_fix

--- src/main/java/core/basesyntax/FootballMatch.java
+++ src/main/java/core/basesyntax/FootballMatch.java
@@ -17,8 +17,7 @@ public class FootballMatch {
     public int matchResult(int result1, int result2, int bet1, int bet2) {
         return (result1 == bet1 && result2 == bet2) ? 2 :
                 ((result1 < result2 && bet1 < bet2)
-                         || (result1 > result2 && bet1 > bet2)
-                            || (result1 == result2 && bet1 == bet2)) ? 1 : 0;
-
+                || (result1 > result2 && bet1 > bet2)
+                || (result1 == result2 && bet1 == bet2)) ? 1 : 0;
     }
 }

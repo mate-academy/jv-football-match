e83fb278bc1d35150582bc68a2b995f41f76d195
SetMyDream <56842438+SetMyDream@users.noreply.github.com>
2020-07-27 13:02:19 +0300

    Now interior variables free!

--- src/main/java/core/basesyntax/FootballMatch.java
+++ src/main/java/core/basesyntax/FootballMatch.java
@@ -2,10 +2,9 @@ package core.basesyntax;
 
 public class FootballMatch {
     public int matchResult(int result1, int result2, int bet1, int bet2) {
-        int ans = ((result1 < result2 && bet1 < bet2)
+        return (result1 == bet1 && result2 == bet2) ? 2 :
+                ((result1 < result2 && bet1 < bet2)
                 || (result1 > result2 && bet1 > bet2)
                 || ((double)result1 / (double)result2 == (double)bet1 / (double)bet2)) ? 1 : 0;
-        ans = (result1 == bet1 && result2 == bet2) ? 2 : ans;
-        return ans;
     }
 }

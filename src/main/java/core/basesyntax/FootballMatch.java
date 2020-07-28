135560c61131ad9e373de53877b20caa3ed4ff92
FYarkiy <flx_jb@yahoo.com>
2020-07-28 12:12:01 +0300

    Simplified code - moved last ternary operation into return and merged declaration of int with first ternary operation

--- src/main/java/core/basesyntax/FootballMatch.java
+++ src/main/java/core/basesyntax/FootballMatch.java
@@ -15,11 +15,9 @@ public class FootballMatch {
      * если не угадал ничего - возвращать 0.</p>
      */
     public int matchResult(int result1, int result2, int bet1, int bet2) {
-        int myResult = 0;
-        myResult = ((result1 > result2) && (bet1 > bet2)
+        int myResult = ((result1 > result2) && (bet1 > bet2)
                 || (result1 < result2) && (bet1 < bet2)
                 || (result1 == result2) && (bet1 == bet2)) ? 1 : 0;
-        myResult = ((result1 == bet1) && (result2 == bet2)) ? 2 : myResult;
-        return myResult;
+        return ((result1 == bet1) && (result2 == bet2)) ? 2 : myResult;
     }
 }

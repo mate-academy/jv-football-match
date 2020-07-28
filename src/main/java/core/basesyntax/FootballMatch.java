45e1964908f0a8cafcfdb9576a4d80877098bde1
vosker <nikita.vaganov16@gmail.com>
2020-07-28 11:43:13 +0300

    removed unnecessary comment

--- src/main/java/core/basesyntax/FootballMatch.java
+++ src/main/java/core/basesyntax/FootballMatch.java
@@ -17,7 +17,7 @@ public class FootballMatch {
     public int matchResult(int result1, int result2, int bet1, int bet2) {
         return result2 == bet2 && result1 == bet1 ? 2 :
                 result1 - result2 == bet1 - bet2
-                        || result1 > result2 && bet1 > bet2  // 3  1   5  3
+                        || result1 > result2 && bet1 > bet2
                         || result1 < result2 && bet1 < bet2 ? 1 : 0;
     }
 }

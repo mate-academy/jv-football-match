44a58d8a92b1470a8fc04ed0c8ff52d8383e9ce3
RomanDubovskyi <rdubowskij@gmail.com>
2022-07-13 18:06:42 +0300

    add missing if block

--- src/main/java/core/basesyntax/FootballMatch.java
+++ src/main/java/core/basesyntax/FootballMatch.java
@@ -16,6 +16,10 @@ public class FootballMatch {
             return 1;
         }
 
+        if (bet1 == bet2 && result1 == result2) {
+            return 1;
+        }
+
         return 0;
     }
 }
